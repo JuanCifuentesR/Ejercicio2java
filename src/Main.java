54c23958e205c6c410ebb3aea365cc27fac3dc33
JuanC <jdiegocifuentes@ucundinamarca.edu.co>
2025-02-13 17:40:04 -0500

    prueba

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Juan Diego Cifuentes");
+        System.out.println("Fonqueta Chia Cundinarca");
+    }
+}
